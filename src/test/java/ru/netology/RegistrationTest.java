7dcfae9664e3ea7f3ca47db122e121f4f7b525ef
AstafevaAnastasia <privet200426@gmail.com>
2025-08-27 19:30:13 +0400

    fix test

--- src/test/java/ru/netology/RegistrationTest.java
+++ src/test/java/ru/netology/RegistrationTest.java
@@ -13,7 +13,6 @@ import static com.codeborne.selenide.Selenide.*;
 public class RegistrationTest {
     @BeforeEach
     void setup() {
-        Configuration.headless = true;
         open("http://localhost:9999");
     }
 
